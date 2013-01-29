f02697141a1726917bb1bcf182588433f7ca4aa0
Mike McDevitt <mcdevitt@bu.edu>
2013-01-29 12:29:20 -0500

    SAT Equivalent Fixed
    Fixed the issue that caused the ACT conversion to throw an error.  The
    SAT conversion was taking a string value as SAT score.  Now the SAT
    score will be the later of the two scores if SAT & ACT scores are both
    input.

--- source/aiInc/calculate.js
+++ source/aiInc/calculate.js
@@ -46,7 +46,7 @@ function getSatEquivalent(){
             document.getElementById('satEquivalent').value = "ACT Score is not valid.";
         }
         {
-            satScore = satScores[actScores.indexOf(actScore)];
+            satScore = +satScores[actScores.indexOf(actScore)];
             document.getElementById('satEquivalent').value = satScore;
             calculateAI();
         }
@@ -54,6 +54,7 @@ function getSatEquivalent(){
 
 function calculateAI(){
     hsGPA = +document.getElementById('hsGPA').value;
+	if ( hsGPA < 1.3 ) { hsGPA = 0; } 	// All GPAs less than 1.3 are treated as F (0.0).
     weighted = document.getElementById('gpaWeighted').checked;
     if (weighted)
         CSR = weightedCSRs[weightedGPAs.indexOf(Math.floor(hsGPA * 10) / 10)];
@@ -62,16 +63,16 @@ function calculateAI(){
     
     if(satScore < satScores[0] || satScore > "1600")
     {
-	document.getElementById('aiResult').innerHTML = "Please give a valid test score.";
-    } else if (hsGPA < 1)
+		document.getElementById('aiResult').innerHTML = "Please give a valid test score.";
+    } else if (document.getElementById('hsGPA').value == "")
         {
             document.getElementById('aiResult').innerHTML = "Please give a valid high school GPA.";
         } else
     {
 		document.getElementById('aiResult').style = "color:";
-        academicIndex = (satScore / 20) + CSR + ( (satScore / 20 ) + CSR ) / 2;
-        //academicIndex = CSR;
-        document.getElementById('aiResult').innerHTML = academicIndex;
+        academicIndex = ( satScore / 20 ) + CSR + ( (satScore / 20 ) + CSR ) / 2;
+		document.getElementById('aiResult').innerHTML = academicIndex;
+        //document.getElementById('aiResult').innerHTML = "hsGPA = " + hsGPA + "( " + satScore + "/ 20 ) + " + CSR + " + ( (" + satScore + " / 20 ) + " + CSR + " ) / 2 = " + academicIndex;  // debugging AI Calculation
     }
         
 
